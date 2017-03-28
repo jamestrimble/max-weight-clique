e05f8988eaf5edf428e01e4054c805e28b8db2e5
James Trimble <james.trimble@yahoo.co.uk>
2017-03-28 20:46:53 +0100

    Include bitset.h in colour_order.c

--- colour_order.c
+++ colour_order.c
@@ -4,6 +4,7 @@
 #include "c_program_timing.h"
 #include "graph.h"
 #include "sorting.h"
+#include "bitset.h"
 
 #include <argp.h>
 #include <limits.h>
@@ -117,38 +118,6 @@ struct UnweightedVtxList {
     int vv[MAX_N];
 };
 
-/*******************************************************************************
-                                Bitset functions
-*******************************************************************************/
-
-static void set_bit(unsigned long long bitset[WORDS_PER_BITSET], int bit) {
-    bitset[bit/BITS_PER_WORD] |= (1ull << (bit%BITS_PER_WORD));
-}
-
-static void unset_bit(unsigned long long bitset[WORDS_PER_BITSET], int bit) {
-    bitset[bit/BITS_PER_WORD] &= ~(1ull << (bit%BITS_PER_WORD));
-}
-
-static int last_set_bit(unsigned long long bitset[WORDS_PER_BITSET], int num_words) {
-    for (int i=num_words-1; i>=0; i--) {
-        if (bitset[i] != 0)
-            return i*BITS_PER_WORD + (BITS_PER_WORD-1-__builtin_clzll(bitset[i]));
-    }
-    return -1;
-}
-
-static void reject_adjacent_vertices(unsigned long long bitset[WORDS_PER_BITSET], unsigned long long adj[WORDS_PER_BITSET],
-        int num_words) {
-    for (int i=0; i<num_words; i++) {
-        bitset[i] &= ~adj[i];
-    }
-}
-
-static void copy_bitset(unsigned long long src[WORDS_PER_BITSET], unsigned long long dest[WORDS_PER_BITSET],
-        int num_words) {
-    for (int i=0; i<num_words; i++)
-        dest[i] = src[i];
-}
 /*******************************************************************************
                                 Graph functions
 *******************************************************************************/
